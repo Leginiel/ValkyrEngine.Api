f98ca25092e358c13f1435c42c03ce7ff5df1a8b
Sebastian Berning <hunter.Vasitas@googlemail.com>
2020-05-10 20:43:04 +0200

    excluding inputsettings from code coverage

--- ValkyrEngine.Api/Input/InputSettings.cs
+++ ValkyrEngine.Api/Input/InputSettings.cs
@@ -1,4 +1,6 @@
-﻿namespace ValkyrEngine.Input
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace ValkyrEngine.Input
 {
   /// <summary>
   /// Specifies the settings that are used to setup the input system.
@@ -8,10 +10,12 @@
     /// <summary>
     /// Determines if the input system listenes to mouse input.
     /// </summary>
+    [ExcludeFromCodeCoverage]
     public bool UseMouseInput { get; }
     /// <summary>
     /// Determines if the input system listenes to keyboard input.
     /// </summary>
+    [ExcludeFromCodeCoverage]
     public bool UseKeyboardInput { get; }
   }
 }
